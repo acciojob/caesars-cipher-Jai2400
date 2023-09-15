5ba8cced2a40c3f22095574cb6f6f688ea79bf36
Jai2400 <117900074+Jai2400@users.noreply.github.com>
2023-09-15 19:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Your Script here.
-
+function rot13(str) {
+  return str.replace(/[A-Z]/g, function(char) {
+    return String.fromCharCode((char.charCodeAt(0) - 65 + 13) % 26 + 65);
+  });
+}
 const lookup = {
   A: "N",
   B: "O",
